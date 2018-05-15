e662119dd1b88fb90a1944de6bbcaace3528283e
Zahid <Zahid.SE@Live.com>
2018-05-15 21:33:05 +0200

    Refactoring experiment script

--- Assignments/02/src/tspExperiment.m
+++ Assignments/02/src/tspExperiment.m
@@ -8,9 +8,11 @@ cityData = importdata('cities.csv');
 nCities = size(cityData.data, 1);
 coords = cityData.data([1:nCities], [3 2])'; % <- switch to plot with north up after imagesc
 
-% plotTsp(output.best(:,end)', coords)
-
+%% Show plot for the fitness
 plot([output.fitMax; output.fitMed]','LineWidth',3);
 legend('Min Distance','Median Distance','Location','NorthWest');
 xlabel('Generations'); ylabel('Distance'); set(gca,'FontSize',16);
 title('Performance on TSP Task')
+
+%% Plot in map
+plotTsp(output.best(:,end)', coords)
